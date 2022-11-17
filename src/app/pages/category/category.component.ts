6030a9e140a705abf57202f728264daa555a3931
HueVu <huevt.jp@gmail.com>
2022-11-17 21:02:57 +0700

    commit code

--- src/app/pages/category/category.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
-})
-
-export class CategoryComponent {}
---
+++ src/app/pages/rings/rings.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './rings.component.html',
+  styleUrls: ['./rings.component.css']
+})
+
+export class RingsComponent {}
