e6cd88bede4b86057442605276e5aa7a21516785
huevu102 <huevt.jp@gmail.com>
2022-11-21 15:00:12 +0700

    commit code

--- src/app/pages/rings/rings.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './rings.component.html',
-  styleUrls: ['./rings.component.css']
-})
-
-export class RingsComponent {}
