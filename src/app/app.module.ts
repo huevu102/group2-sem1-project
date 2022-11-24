ddb431f28b9321986060bcb8b0803dca32d20062
Nthuyduong <nguyenthuyduong15113@gmail.com>
2022-11-24 19:12:56 +0700

    shop all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeANComponent} from "./pages/home-AN-TEST/home-AN.component";
