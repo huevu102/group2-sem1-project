d5dc9cbbab076df96ea842f1ca9a1274dbaff910
TruongAnNguyenVan <109612341+TruongAnNguyenVan@users.noreply.github.com>
2022-11-25 11:17:46 +0700

    a

--- src/assets/img/home/javascript.js
+++ src/assets/img/home/javascript.js
@@ -9,10 +9,10 @@ function Slide(title,link){
   slideArray.push(this);
 }
 
-var no1 = new Slide("End-Off-Year Collection","../../../assets/img/home-AN-TEST/39cfa61261a1d318e1bbe72ada8e6dcb 2.png");
-var no2 = new Slide("Sweet Collection","../../../assets/img/home-AN-TEST/39cfa61261a1d318e1bbe72ada8e6dcb 21.png");
-var no3 = new Slide("Minimal Collection","../../../assets/img/home-AN-TEST/39cfa61261a1d318e1bbe72ada8e6dcb 22.png");
-var no4 = new Slide(" ","../../../assets/img/home-AN-TEST/39cfa61261a1d318e1bbe72ada8e6dcb 23.png");
+var no1 = new Slide("End-Off-Year Collection","../../../assets/img/home/39cfa61261a1d318e1bbe72ada8e6dcb 2.png");
+var no2 = new Slide("Sweet Collection","../../../assets/img/home/39cfa61261a1d318e1bbe72ada8e6dcb 21.png");
+var no3 = new Slide("Minimal Collection","../../../assets/img/home/39cfa61261a1d318e1bbe72ada8e6dcb 22.png");
+var no4 = new Slide(" ","../../../assets/img/home/39cfa61261a1d318e1bbe72ada8e6dcb 23.png");
 
 function sort(){
   var x = slideArray[0];
@@ -38,6 +38,15 @@ function nextSlide(){
   var tit = document.getElementById("tittle");
   var text = slideArray[2].title;
   tit.innerHTML = text;
+}
+
+function slide(){
+  var x1 = document.getElementById('mySlider');
+  x1.style.opacity = '0';
+  setTimeout(()=>{
+    nextSlide();
+    x1.style.opacity = '1';
+  },400);
 }
 
 
