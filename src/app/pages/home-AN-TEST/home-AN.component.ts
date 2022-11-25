e132708b2054a14db8363478b2bde4c37554f62b
TruongAnNguyenVan <109612341+TruongAnNguyenVan@users.noreply.github.com>
2022-11-25 11:33:56 +0700

    a

--- src/app/pages/home-AN-TEST/home-AN.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: 'app-home-AN-TEST',
-  templateUrl: './home-AN.component.html',
-  styleUrls: ['./home-AN.component.css']
-})
-
-export class HomeANComponent {}
