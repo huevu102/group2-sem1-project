f0e177ad9824829f0a861c561eccc18a2a15e0b9
Nthuyduong <nguyenthuyduong15113@gmail.com>
2022-11-27 20:32:01 +0700

    shop all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,19 @@ export class AppComponent {
   title = 'group2-sem1-project';
 
   constructor(){
+    this.function()
+  }
+  function(){
+    const nav = document.querySelector(".headnav");
+    let lastScrollY = window.scrollY;
+    window.addEventListener("scroll",() => {
+      if (lastScrollY < window.scrollY){
+        nav?.classList.add("nav--hidden");
+      }
+      else{
+        nav?.classList.remove("nav--hidden");
+      }
+      lastScrollY = window.scrollY;
+    });
   }
 }
