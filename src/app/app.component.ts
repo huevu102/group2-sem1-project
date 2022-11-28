a1c69236d2891ebc712d890e75cc1406d946d55a
Nthuyduong <nguyenthuyduong15113@gmail.com>
2022-11-28 14:31:42 +0700

    shop all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'group2-sem1-project';
 
+  /*THIS FOR SCROLL NAVBAR*/
   constructor(){
-    this.function()
+    this.function();
   }
 
   function(){
