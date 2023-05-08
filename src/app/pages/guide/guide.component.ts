f22f538a480ec7cb549ce494f6393e803f72233d
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-05-08 13:41:48 +0700

    guide page

--- src/app/pages/guide/guide.component.ts
+++ src/app/pages/guide/guide.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-guide',
@@ -6,4 +7,17 @@ import {Component} from "@angular/core";
   styleUrls: ['./guide.component.css']
 })
 
-export class GuideComponent {}
+export class GuideComponent {
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset !== 0;
+    });
+  }
+  windowScrolled = false;
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
+}
